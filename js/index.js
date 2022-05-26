9882a643e843545b1b16556f8f952c28263677b3
Maju Testoni <majuliatestoni@gmail.com>
2022-05-26 20:16:44 -0300

    certo

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ function trocarJogador() {
 
 for (let i = 0; i < 9; i++) {
     tabuleiro[i].addEventListener('click', (event) => {
-        if (event.target.value == '' && vencedor == '') {
+        if (event.target.value == " " && vencedor == '') {
             event.target.value = jogador;
             event.target.style.color = 'white';
 
@@ -51,7 +51,7 @@ for (let i = 0; i < 9; i++) {
 
 reinicirJogo.addEventListener('click', () => {
     for (let i = 0; i < 9; i++) {
-        tabuleiro[i].value = '';
+        tabuleiro[i].value = " ";
         tabuleiro[i].style.color = 'white'
         tabuleiro[i].style.backgroundColor = 'rgb(44, 110, 47)'
 
@@ -67,55 +67,55 @@ reinicirJogo.addEventListener('click', () => {
 
 
 let vitoria = function () {
-    if ((tabuleiro[0].value == tabuleiro[1].value) && (tabuleiro[1].value == tabuleiro[2].value) && tabuleiro[0].value != '') {
+    if ((tabuleiro[0].value == tabuleiro[1].value) && (tabuleiro[1].value == tabuleiro[2].value) && tabuleiro[0].value != " ") {
         tabuleiro[0].style.backgroundColor = '#0F0';
         tabuleiro[1].style.backgroundColor = '#0F0';
         tabuleiro[2].style.backgroundColor = '#0F0';
 
         return tabuleiro[0].value;
 
-    } else if ((tabuleiro[3].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[5].value) && tabuleiro[3].value != '') {
+    } else if ((tabuleiro[3].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[5].value) && tabuleiro[3].value != " ") {
         tabuleiro[3].style.backgroundColor = '#0F0';
         tabuleiro[4].style.backgroundColor = '#0F0';
         tabuleiro[5].style.backgroundColor = '#0F0';
 
         return tabuleiro[3].value;
 
-    } else if ((tabuleiro[6].value == tabuleiro[7].value) && (tabuleiro[7].value == tabuleiro[8].value) && tabuleiro[6].value != '') {
+    } else if ((tabuleiro[6].value == tabuleiro[7].value) && (tabuleiro[7].value == tabuleiro[8].value) && tabuleiro[6].value != " ") {
         tabuleiro[6].style.backgroundColor = '#0F0';
         tabuleiro[7].style.backgroundColor = '#0F0';
         tabuleiro[8].style.backgroundColor = '#0F0';
 
         return tabuleiro[6].value;
 
-    } else if ((tabuleiro[0].value == tabuleiro[3].value) && (tabuleiro[3].value == tabuleiro[6].value) && tabuleiro[0].value != '') {
+    } else if ((tabuleiro[0].value == tabuleiro[3].value) && (tabuleiro[3].value == tabuleiro[6].value) && tabuleiro[0].value != " ") {
         tabuleiro[0].style.backgroundColor = '#0F0';
         tabuleiro[3].style.backgroundColor = '#0F0';
         tabuleiro[6].style.backgroundColor = '#0F0';
 
         return tabuleiro[0].value;
 
-    } else if ((tabuleiro[1].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[7].value) && tabuleiro[1].value != '') {
+    } else if ((tabuleiro[1].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[7].value) && tabuleiro[1].value != " ") {
         tabuleiro[1].style.backgroundColor = '#0F0';
         tabuleiro[4].style.backgroundColor = '#0F0';
         tabuleiro[7].style.backgroundColor = '#0F0';
 
         return tabuleiro[1].value;
 
-    } else if ((tabuleiro[2].value == tabuleiro[5].value) && (tabuleiro[5].value == tabuleiro[8].value) && tabuleiro[2].value != '') {
+    } else if ((tabuleiro[2].value == tabuleiro[5].value) && (tabuleiro[5].value == tabuleiro[8].value) && tabuleiro[2].value != " ") {
         tabuleiro[2].style.backgroundColor = '#0F0';
         tabuleiro[5].style.backgroundColor = '#0F0';
         tabuleiro[8].style.backgroundColor = '#0F0';
 
         return tabuleiro[2].value;
-    } else if ((tabuleiro[0].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[8].value) && tabuleiro[0].value != '') {
+    } else if ((tabuleiro[0].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[8].value) && tabuleiro[0].value != " ") {
         tabuleiro[0].style.backgroundColor = '#0F0';
         tabuleiro[4].style.backgroundColor = '#0F0';
         tabuleiro[8].style.backgroundColor = '#0F0';
 
         return tabuleiro[0].value;
 
-    } else if ((tabuleiro[2].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[6].value) && tabuleiro[2].value != '') {
+    } else if ((tabuleiro[2].value == tabuleiro[4].value) && (tabuleiro[4].value == tabuleiro[6].value) && tabuleiro[2].value != " ") {
         tabuleiro[2].style.backgroundColor = '#0F0';
         tabuleiro[4].style.backgroundColor = '#0F0';
         tabuleiro[6].style.backgroundColor = '#0F0';
